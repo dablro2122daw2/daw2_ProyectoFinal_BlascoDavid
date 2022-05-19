6df7201dc87f99638b0bde7c54ed4bacb4daf950
unknown <15583039.clot@fje.edu>
2022-05-19 19:50:49 +0200

    17e Commit. Cambio Logotipo en Partida

--- JuegoCartas/resources/views/Jugar.blade.php
+++ JuegoCartas/resources/views/Jugar.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Joc de Cartes</title>
+    <title>Square Conquest - Partida Iniciada</title>
     <link rel="stylesheet" href="/css/header.css"/>
     <link rel="stylesheet" href="/css/game.css"/>
     <script src="https://cdn.socket.io/4.5.0/socket.io.min.js" integrity="sha384-7EyYLQZgWBi67fBtVxw60/OWl1kjsfrPFcaU0pp0nAh+i8FD068QogUvg85Ewy1k" crossorigin="anonymous"></script>
@@ -12,10 +12,12 @@
 </head>
 <body>
 <div class="header">
-      <div class="imagenesMenu1"><img id="express" src="/imgs/express.png"/><img id="websockets" src="/imgs/sockets.png"/></div>
-      <div class="imagenesMenu2"><img id="pug" src="/imgs/pug.png"/></div>
+      <div class="imagenesMenu1"></div>
+      <div class="imagenesMenu1"><img id="pug" src="/imgs/pug.png"/></div>
     </div>
-    <h4 id="timer">0</h4>
+    <div id="timer">
+      <button onclick="location.href='/MenuJugador'" id="timer">Abandonar Partida</button>
+    </div>
     <div class="divGame">
       <div class="jugadorTu">
         <h4>Jugador 1</h4><img id="user" src="/imgs/user.png"/>
@@ -65,10 +67,6 @@
         <h4>Jugador 2</h4><img id="user" src="/imgs/user2.png"/>
         <div id="colorJugadorContrario"></div>
         <h2 id="puntuacionVerde">0</h2>
-      </div>
-      </p>
-      <div>
-        <button onclick="location.href='/MenuJugador'" id="1">Abandonar Partida</button>
       </div>
     </div>
 </body>
